2f42b26dcff9d78d5843ccdd75f584c313a9603e
FilipeHSAraujo <filipehsaraujo@gmail.com>
2021-07-23 18:47:18 -0300

    alpha

---
+++ pages/index.js
@@ -0,0 +1,49 @@
+//imports
+//Page
+export default function index() {
+return(
+<div className="Main">
+ <body>
+
+   <head>
+    <title>Água</title>
+    <meta name="description" content="to catucando tudo" />
+    <link rel="icon" href="/favicon.ico" />
+   </head>
+  
+  <div className="Title">
+      <h1> Soluções para a Água</h1>
+      <h3 > com o grupo 3</h3>
+  </div>
+  
+<div className="Navbar">
+<ul>
+  <li><a href="#home">Inicio</a></li>
+  <li><a href="#map">Mapa</a></li>
+  <li><a href="#download">Download</a></li>
+</ul>
+
+</div>
+
+  <div className="Body">
+<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec eget mollis nibh. Nam luctus tristique nulla, eu malesuada risus condimentum eget. Suspendisse quis dui nisi. Aenean eget lorem euismod, pretium tortor nec, interdum lorem. Interdum et malesuada fames ac ante ipsum primis in faucibus. Vivamus posuere, leo nec pretium porttitor, tellus mauris laoreet augue, in aliquam lorem odio quis nisi. Donec id velit urna. Ut non nunc justo. Curabitur elementum porttitor dui nec mattis. Pellentesque nulla neque, feugiat nec feugiat ac, tempor vehicula urna. Donec aliquet pharetra neque, vel blandit nisl efficitur in. Vestibulum vitae ipsum id massa ultrices interdum non vehicula tellus.</p>
+
+<p>Sed aliquet dapibus eros ut faucibus. Sed nec elit a ante dignissim cursus id at sem. Integer nec cursus nisi. Donec id vestibulum felis, feugiat pretium neque. Pellentesque aliquet arcu eget iaculis aliquet. Donec bibendum semper tristique. Sed mollis turpis et metus elementum ultrices. Quisque faucibus interdum nisl. Aenean faucibus nibh vel libero laoreet dapibus. Duis fringilla massa sed nulla euismod, non mollis ex porttitor.</p>
+
+<p>Suspendisse in mattis enim, at dictum dolor. Aenean vel metus risus. Aenean id turpis tristique, euismod felis in, ultricies felis. Morbi vitae ligula tellus. Vestibulum rhoncus ornare cursus. Morbi in diam ut erat mattis sagittis. Morbi eu massa ante.</p>
+
+<p>Pellentesque iaculis varius lobortis. Cras eget imperdiet tellus, non elementum lorem. Proin nisl justo, consequat ac laoreet sit amet, euismod sit amet libero. Quisque sodales augue eget feugiat condimentum. Maecenas pulvinar hendrerit nisl, et vehicula justo placerat scelerisque. Suspendisse non magna in massa ultrices bibendum vel et lorem. Aenean commodo massa vitae tortor interdum porta.</p>
+
+<p>Maecenas suscipit luctus lorem non blandit. Maecenas quam metus, faucibus ac urna id, efficitur sollicitudin enim. Maecenas porta elit et nisi rutrum feugiat. Duis vestibulum consectetur quam, bibendum pellentesque eros porttitor et. Mauris et lacinia sem. Fusce at feugiat velit. Proin pretium semper massa, nec volutpat quam malesuada a. Nulla in tempor mi, dapibus sodales est.</p>
+
+  </div>
+  
+  <div className="Footer">
+    <h6> Feito pelo Grupo 3</h6>
+
+  </div>
+  
+   </body>
+  </div>
+ )
+}
